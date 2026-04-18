fb67c70234db07da4014e7d2be83304d36c22d0b
Shikhar singh <shikhar482006@gmail.com>
2026-04-18 21:46:29 +0530

    chore: integrate user provided index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
+/**
+ * index.tsx - React Application Entry Point
+ * Mount the MentorMatch app to the DOM
+ */
+ 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MentorMatch from './App';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ 
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+ 
+root.render(
   <React.StrictMode>
     <MentorMatch />
   </React.StrictMode>
 );
+ 
+export default MentorMatch;
